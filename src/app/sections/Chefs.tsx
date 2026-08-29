5eace8dfe7438beabf99fbeacec4a161d47e1473
Ron Cedric Geronimo <grb1544@dlsud.edu.ph>
2026-08-29 15:51:31 +0800

    removed fetch directories

--- src/app/sections/Chefs.tsx
+++ src/app/sections/Chefs.tsx
@@ -1,33 +1,26 @@
-import React from 'react'
+import React from 'react';
 import SectionTitle from '../components/SectionTitle';
 import ChefsItem from '../components/ChefsItem';
+import { chefs } from '../data/data';
 
-async function getChefsData() {
-    const res = await fetch('/api/chefs');
-    return res.json();
-}
-
-export default async function Chefs() {
-    const items: [] = await getChefsData();
-
+export default function Chefs() {
     return (
         <section id="chefs" className="chefs">
             <div className="container" data-aos="fade-up">
-                <SectionTitle title="Chefs" subtitle="Meet the Culinary Masters"/>
+                <SectionTitle
+                    title="Chefs"
+                    subtitle="Meet the Culinary Masters"
+                />
+
                 <div className="row">
-                    {items &&
-                        items.length > 0 &&
-                        items.map(
-                            (item: {
-                                id: number;
-                                name: string;
-                                photo: string;
-                                position: string;
-                                delay: string;
-                            }) => <ChefsItem key={item.id} item={item} />
-                        )}
+                    {chefs.map((item) => (
+                        <ChefsItem
+                            key={item.id}
+                            item={item}
+                        />
+                    ))}
                 </div>
             </div>
         </section>
-    )
+    );
 }
--- src/app/sections/WhyUs.tsx
+++ src/app/sections/WhyUs.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 import './whyUs.css';
 import WhyUsCard from '../components/WhyUsCard';
 import SectionTitle from '../components/SectionTitle';
+import { whyUs } from '../data/data';
 
-async function getWhyUsData() {
-    const res = await fetch('/api/whyus');
-    return res.json();
-}
-
-export default async function WhyUs() {
-
-    const items: [] = await getWhyUsData();
-
+export default function WhyUs() {
     return (
         <section id="why-us" className="why-us">
             <div className="container">
-                <SectionTitle title="Why Us" subtitle="Why Choose Our Restaurant"/>
+                <SectionTitle
+                    title="Why Us"
+                    subtitle="Why Choose Our Restaurant"
+                />
+
                 <div className="row">
-                    {items && items.length > 0 && items.map(
-                        (item: { id: number; title: string; content: string }) => (
-                            <WhyUsCard key={item.id} item={item} /> 
+                    {whyUs.map(
+                        (item: {
+                            id: number;
+                            title: string;
+                            content: string;
+                        }) => (
+                            <WhyUsCard
+                                key={item.id}
+                                item={item}
+                            />
                         )
                     )}
                 </div>
             </div>
         </section>
-    )
+    );
 }
